395b2d35eeb5c56260aa526ada24088df4931804
chrishansen69 <cadrekucra@gmx.com>
2017-06-03 23:58:40 -0300

    gz to xz

--- install_node.sh
+++ install_node.sh
@@ -4,8 +4,8 @@ if [ $PWD != '/home/jitpack/build' ]; then
 fi
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://nodejs.org/dist/v8.0.0/node-v8.0.0-linux-x64.tar.xz
-tar xvf *.tar.gz
-rm -f *.tar.gz
+tar xvf *.tar.xz
+rm -f *.tar.xz
 
 #mv node-v8.0.0-linux-x64/.* .
 #mv node-v8.0.0-linux-x64/* .
